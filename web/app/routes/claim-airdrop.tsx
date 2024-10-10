92a1e424912cfceafb6e420785fe47367f259020
Nikita <github@nikiv.dev>
2024-10-10 22:06:53 +0200

    .

--- web/app/routes/claim-airdrop.tsx
+++ web/app/routes/claim-airdrop.tsx
@@ -9,7 +9,7 @@ import {
   setAirdropWalletForClaimAsClaimed,
 } from "~/actions"
 import { ClientOnly } from "~/lib/react"
-import useBlockchainActions, {toNanoDigits} from "../lib/airdrop/useActions"
+import useBlockchainActions, { toNanoDigits } from "../lib/airdrop/useActions"
 import { set } from "ronin"
 
 function RouteComponent() {
@@ -35,76 +35,80 @@ function RouteComponent() {
           <h2 className="text-2xl font-bold ">Claim Airdrop</h2>
           <TonConnectButton />
         </div>
-        <div className="space-y-4">
-          {data?.map((airdrop) => (
-            <div key={airdrop.id}>
-              <p
-                className={`
-                relative overflow-hidden px-6 py-3 rounded-full
-                bg-gradient-to-r from-purple-500 to-pink-500
+        {data?.map((airdrop) => (
+          <div
+            key={airdrop.id}
+            className="p-2 bg-gradient-to-r space-x-2 from-purple-500 to-pink-500 rounded-lg flex flex-row items-center"
+          >
+            <img
+              className="w-7 h-7 rounded-full"
+              src={airdrop.image}
+              alt={airdrop.jettonName}
+            />
+            <p
+              className={`
+                relative overflow-hidden
+
                 text-white font-bold text-lg
                 transform transition-all duration-300 ease-in-out cursor-pointer
               `}
-                onClick={async () => {
-                  try {
-                    const entries = await getEntriesForAirdrop({
-                      airdropAddress: airdrop.airdropAddress,
-                    })
-                    console.log(entries, "entries")
-                    // @ts-ignore
-                    const parsedEntries = entries.map((entry) => ({
-                      address: Address.parse(entry.walletAddress),
-                      amount: BigInt(entry.tokenAmount)//, +airdrop.digits),
-                    }))
+              onClick={async () => {
+                try {
+                  const entries = await getEntriesForAirdrop({
+                    airdropAddress: airdrop.airdropAddress,
+                  })
+                  console.log(entries, "entries")
+                  // @ts-ignore
+                  const parsedEntries = entries.map((entry) => ({
+                    address: Address.parse(entry.walletAddress),
+                    amount: BigInt(entry.tokenAmount), //, +airdrop.digits),
+                  }))
 
-                    console.log(parsedEntries, "parsedEntries")
-                    console.log(airdrop.airdropAddress, "testing..")
-                    await claimAirdrop({
-                      airdropAddress: Address.parse(airdrop.airdropAddress),
-                      entries: parsedEntries,
-                    })
-                    await fetch(
-                      "http://localhost:8787/set-airdrop-wallet-for-claim-as-claimed",
-                      {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                          airdropAddress: airdrop.airdropAddress,
-                          walletAddress: address,
-                          // @ts-ignore
-                          entries: entries.map((entry) => ({
-                            address: entry.walletAddress,
-                            amount: entry.tokenAmount.toString(),
-                          })),
-                        }),
+                  console.log(parsedEntries, "parsedEntries")
+                  console.log(airdrop.airdropAddress, "testing..")
+                  await claimAirdrop({
+                    airdropAddress: Address.parse(airdrop.airdropAddress),
+                    entries: parsedEntries,
+                  })
+                  await fetch(
+                    "http://localhost:8787/set-airdrop-wallet-for-claim-as-claimed",
+                    {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
                       },
-                    )
-                    // await setAirdropWalletForClaimAsClaimed({
-                    //   airdropAddress: airdrop.airdropAddress.toString(),
-                    //   walletAddress: address,
-                    //   // @ts-ignore
-                    //   entries: entries?.map((entry) => ({
-                    //     address: entry.walletAddress,
-                    //     amount: entry.tokenAmount,
-                    //   })),
-                    // })
-                  } catch (err) {
-                    console.log(err, "err")
-                  }
-                }}
-              >
-                Claim before:{" "}
-                <strong>
-                  {/* @ts-ignore */}
-                  {formatDistanceToNow(new Date(airdrop.endDate))}
-                </strong>{" "}
-                passes
-              </p>
-            </div>
-          ))}
-        </div>
+                      body: JSON.stringify({
+                        airdropAddress: airdrop.airdropAddress,
+                        walletAddress: address,
+                        // @ts-ignore
+                        entries: entries.map((entry) => ({
+                          address: entry.walletAddress,
+                          amount: entry.tokenAmount.toString(),
+                        })),
+                      }),
+                    },
+                  )
+                  // ..
+                  const res = await setAirdropWalletForClaimAsClaimed({
+                    airdropAddress: airdrop.airdropAddress.toString(),
+                    walletAddress: address,
+                    // @ts-ignore
+                    entries: entries?.map((entry) => ({
+                      address: entry.walletAddress,
+                      amount: entry.tokenAmount,
+                    })),
+                  })
+                  // ..
+                } catch (err) {
+                  console.log(err, "err")
+                }
+              }}
+            >
+              Claim {airdrop.jettonName} before {/* @ts-ignore */}
+              {formatDistanceToNow(new Date(airdrop.endDate))}
+            </p>
+          </div>
+        ))}
         {/* <button
           onClick={() => {
             // claimAirdrop({ airdropAddress: airDropAddress, entries })
