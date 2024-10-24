5a45654e6877d625c07034e5c3c82b6658c32e43
Nikita <github@nikiv.dev>
2024-10-24 17:46:06 +0200

    try do worker for checking if split completed

--- worker/jetton-split-worker.ts
+++ worker/jetton-split-worker.ts
@@ -1,4 +1,4 @@
-import { Address } from "@ton/core"
+import { Address, OpenedContract, Transaction } from "@ton/core"
 import { tonClient } from "./lib/ton-sender"
 import { getEnvOrThrow } from "./lib/env"
 import { SampleJetton } from "./lib/ton-master"
@@ -6,15 +6,46 @@ import { SampleJetton } from "./lib/ton-master"
 // TODO: have a worker that checks that jetton split tx, actually worked, after that do ronin db call
 // worker.ts code is similar in that it checks whether token was burned or not succesfully
 
-const masterAddress = process.env.MASTER_ADDRESS!
-
 async function main() {
   try {
     const masterContract = tonClient.open(
       SampleJetton.fromAddress(Address.parse(getEnvOrThrow("MASTER_ADDRESS"))),
     )
+    const childAddress = await masterContract.getGetWalletAddress(
+      Address.parse(""),
+    )
+    while (true) {
+      console.log("Checking for new transactions...")
+      try {
+        await checkForNewTransactions(childAddress, masterContract)
+      } catch (error) {
+        console.log(error, "error")
+      }
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    }
   } catch (err) {
     console.log(err, "error")
+  }
+}
+
+async function checkForNewTransactions(
+  childAddress: Address,
+  master: OpenedContract<SampleJetton>,
+) {
+  let i = 20
+  while (i >= 0) {
+    let transactions: Transaction[]
+    try {
+      transactions = await tonClient.getTransactions(childAddress, {
+        limit: i,
+      })
+    } catch (e) {
+      i--
+      await new Promise((r) => setTimeout(r, 500))
+      continue
+    }
+    for (const tr of transactions) {
+    }
   }
 }
 
